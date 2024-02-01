a778abcb4cdbcba968cd67e561643e6afddffd2b
Julia Cottingham <115940523+juliacottingham@users.noreply.github.com>
2024-02-01 17:43:57 -0500

    Delete python_scripts directory

--- python_scripts/test.sh
+++
@@ -1,1 +0,0 @@
-python3 -i new_final6.py /home/labwork/autocontrol/output_files/led_v_curve36/processed_data occupancy.npy 1 fit_param.npy
--- python_scripts/test4.sh
+++
@@ -1,1 +0,0 @@
-python3 -i new_final8.py /home/labwork/autocontrol/output_files/led_signal3/processed_data occupancy.npy 1 fit_param.npy
